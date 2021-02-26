216ee4e2f1cad9e5a6ea4896863ac13ac01f7f95
waberto <waberi.houssein@gmail.com>
2021-02-27 00:02:21 +0100

    Implementation entities and mapping

---
+++ MaBanque/src/main/java/com/demo/MaBanque/MaBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.demo.MaBanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaBanqueApplication.class, args);
+	}
+
+}
---
+++ MaBanque/src/test/java/com/demo/MaBanque/MaBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.MaBanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
